51871d9bb813c6bee94bfb6a27d24b34568c9696
Erik Jensen <erikalanj@gmail.com>
2024-09-11 18:50:06 -0400

    Create Account.java

---
+++ Account.java
@@ -0,0 +1,56 @@
+public class Account extends AccessAccount {
+
+  private String name;
+
+  private static String dob;
+
+  private int accountID;
+
+  public Account(String name, String dob) {
+
+    super(name, getLastAccountNumber(), dob);
+
+    this.name = name;
+
+    this.dob = dob;
+
+    if (checkAccount(name)) {
+
+      System.out.println("Account already exists for this user.");
+
+    } else {
+
+      createAccount(name, dob);
+
+      this.accountID = getAccountID(); // Use the getter method to get accountID
+
+      System.out.println("Account created successfully.");
+
+    }
+
+  }
+
+  public Account(String name, String dob, double initialDeposit) {
+
+    this(name, dob);
+
+    deposit(dob, initialDeposit);
+
+  }
+
+  public Account(String name, String password, String operation, double amount) {
+
+    super(name, getLastAccountNumber(), dob); // Use this.dob to refer to the instance variable
+
+    if (operation.equals("deposit")) {
+
+      deposit(this.dob, amount);
+
+    } else if (operation.equals("withdraw")) {
+
+      withdraw(this.dob, amount);
+
+    }
+
+  }
+}
